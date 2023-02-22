72931e617ee0786e370da1474fb89c6a3ab706c0
unknown <halhhute@gmail.com>
2023-02-22 17:30:37 +0700

    Upload Js task 1=>6

--- js/index.js
+++ js/index.js
@@ -1,1 +1,92 @@
-// console.log('xyz');
+//NHẬP SỐ TẠO MẢNG
+var arrNum = [];
+document.getElementById('btn-tao-mang').onclick = function () {
+    var num = document.getElementById('num').value;
+    if (num === '') {
+        num = 0;
+    }
+    arrNum.push(Number(num));
+    // Number(arrNum)
+    document.getElementById('hien-thi-mang').innerHTML = '[ ' + arrNum + ' ]';
+
+}
+
+//Bài tập 1: Tổng các số dương trong mảng
+document.getElementById('btn-bai-1').onclick = function () {
+    //input: arrNum, 
+
+    //output : ketQua(number);
+    var ketQua = 0;
+    //process:
+    ketQua = tongSoDuong(arrNum);
+    document.getElementById('ket-qua-bai-1').innerHTML = 'Tổng số dương là: ' + ketQua;
+}
+
+//Bài tập 2: Đếm số dương trong mảng
+document.getElementById('btn-bai-2').onclick = function () {
+    //input: arrNum
+    //output: ketQua(number)
+    var ketQua = 0;
+    //process:
+    ketQua = demSoDuong(arrNum);
+    document.getElementById('ket-qua-bai-2').innerHTML = 'Số lượng số Dương trong mảng là: ' + ketQua;
+}
+
+//Bài tập 3: Tìm số nhỏ nhất trong mảng
+document.getElementById('btn-bai-3').onclick = function () {
+    //input: arrNum
+    //output: ketQua (number)
+    var ketQua = '';
+    //process
+    ketQua = timSoNhoNhat(arrNum);
+    
+    document.getElementById('ket-qua-bai-3').innerHTML = 'Số nhỏ nhất của mảng là: ' + ketQua;   
+    
+}
+
+//Bài tập 4: tìm số dương nhỏ nhất trong mảng
+document.getElementById('btn-bai-4').onclick = function(){
+    //input: arrNum
+    //output: ketQua
+    var ketQua = '';
+    //process:
+    ketQua = soDuongNhoNhat(arrNum);
+    if (ketQua===-1){
+        document.getElementById('ket-qua-bai-4').innerHTML = 'Mảng không có số dương!';
+    } else {
+        document.getElementById('ket-qua-bai-4').innerHTML = 'Số dương nhỏ nhất của mảng là: ' + ketQua;
+    }
+}
+
+//Bài tập 5: Tìm số chẵn cuối cùng
+document.getElementById('btn-bai-5').onclick = function(){
+    //input: arrNum
+    //output: ketQua
+    var ketQua = '';
+    //process:
+    ketQua = soChanCuoiCung (arrNum);
+    if (ketQua===-1){
+        document.getElementById('ket-qua-bai-5').innerHTML = ketQua;
+    } else {
+        document.getElementById('ket-qua-bai-5').innerHTML = 'Số chẵn cuối cùng của mảng là: ' + ketQua;
+    }
+}
+
+//Bài tập 6: Đổi chỗ 2 vị trí trong mảng
+document.getElementById('btn-bai-6').onclick = function(){
+    //input: arrNum, viTri1(number), viTri2(number)
+    var viTri1 = document.getElementById('vi-tri-1').value;
+    var viTri2 = document.getElementById('vi-tri-2').value;
+    //output: ketQua
+    
+    //process:
+    if (viTri1===''||viTri2===''){
+        document.getElementById('ket-qua-bai-6').innerHTML ='Vui lòng nhập vị trí cần đổi!'
+    } else if (viTri1>=arrNum.length||viTri2>=arrNum.length){
+        document.getElementById('ket-qua-bai-6').innerHTML = 'Vị trí không khả dụng! Mảng hiện tại là: ' + '[ ' + arrNum + ' ]';
+    } else {
+        doiViTri(arrNum,viTri1,viTri2);
+        document.getElementById('ket-qua-bai-6').innerHTML = '[ ' + arrNum + ' ]';
+    }
+    
+}
--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,75 @@
-// console.log('abc');
+/**
+ * Hàm tính tổng các số dương trong mảng
+ * @param {*} arr mảng người dùng nhập vào
+ * @returns output: kết quả tính tổng các số dương trong mảng
+ */
+function tongSoDuong(arr) {
+    var output = 0;
+    for (var index = 0; index < arr.length; index++) {
+        if (arr[index] >= 0) {
+            output += +arr[index];
+        }
+    }
+    return output;
+}
+/**
+ * Hàm đếm số lượng số dương có trong mảng
+ * @param {*} arr mảng người dùng nhập vào
+ * @returns số lượng số dương
+ */
+function demSoDuong(arr) {
+    var output = 0;
+    for (var index = 0; index < arr.length; index++) {
+        if (arr[index] > 0) {
+            output++;
+        }
+    }
+    return output;
+}
+
+function timSoNhoNhat(arr) {
+    var output = arr[0];
+    for (var index = 1; index < arr.length; index++) {
+        if (arr[index] < output) {
+            output = arr[index];
+        }
+    }
+    return output;
+}
+/**
+ * Hàm tìm số dương nhỏ nhất trong mảng
+ * @param {*} arr mảng người dùng nhập vào
+ * @returns hàm trả về giá trị output là số dương nhỏ nhất hoặc "-1" (trường hợp k có số dương)
+ */
+function soDuongNhoNhat(arr) {
+    var output = -1;
+    for (var index = 0; index < arr.length; index++) {
+        if ((output === -1 || output > arr[index]) && arr[index] > 0) {
+            output = arr[index];
+        }
+    }
+    return output;
+}
+/**
+ * Hàm tìm số chẵn cuối cùng trong mảng
+ * @param {*} arr mảng người dùng nhập vào
+ * @returns hàm trả về giá trị số chẵn cuối cùng hoặc -1(trường hợp không có số chẵn)
+ */
+function soChanCuoiCung(arr) {
+    var output = -1;
+    for (var index = 0; index < arr.length; index++){
+        if (arr[index]%2===0){
+            output = arr[index];
+        }
+    }
+    return output;
+}
+
+function doiViTri(arr,num1,num2){
+    var output='';
+    var giaTri1 = arr[num1];
+    var giaTri2 = arr[num2]
+    arr[num1]=giaTri2;
+    arr[num2]=giaTri1;
+    output = arr;
+}
