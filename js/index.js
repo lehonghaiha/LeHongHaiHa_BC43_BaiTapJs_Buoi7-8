a0e262893c3367aae6fd4e683b58f9af53b2226e
unknown <halhhute@gmail.com>
2023-02-24 16:33:21 +0700

    Done Js

--- js/index.js
+++ js/index.js
@@ -39,19 +39,19 @@ document.getElementById('btn-bai-3').onclick = function () {
     var ketQua = '';
     //process
     ketQua = timSoNhoNhat(arrNum);
-    
-    document.getElementById('ket-qua-bai-3').innerHTML = 'Số nhỏ nhất của mảng là: ' + ketQua;   
-    
+
+    document.getElementById('ket-qua-bai-3').innerHTML = 'Số nhỏ nhất của mảng là: ' + ketQua;
+
 }
 
 //Bài tập 4: tìm số dương nhỏ nhất trong mảng
-document.getElementById('btn-bai-4').onclick = function(){
+document.getElementById('btn-bai-4').onclick = function () {
     //input: arrNum
     //output: ketQua
     var ketQua = '';
     //process:
     ketQua = soDuongNhoNhat(arrNum);
-    if (ketQua===-1){
+    if (ketQua === -1) {
         document.getElementById('ket-qua-bai-4').innerHTML = 'Mảng không có số dương!';
     } else {
         document.getElementById('ket-qua-bai-4').innerHTML = 'Số dương nhỏ nhất của mảng là: ' + ketQua;
@@ -59,13 +59,13 @@ document.getElementById('btn-bai-4').onclick = function(){
 }
 
 //Bài tập 5: Tìm số chẵn cuối cùng
-document.getElementById('btn-bai-5').onclick = function(){
+document.getElementById('btn-bai-5').onclick = function () {
     //input: arrNum
     //output: ketQua
     var ketQua = '';
     //process:
-    ketQua = soChanCuoiCung (arrNum);
-    if (ketQua===-1){
+    ketQua = soChanCuoiCung(arrNum);
+    if (ketQua === -1) {
         document.getElementById('ket-qua-bai-5').innerHTML = ketQua;
     } else {
         document.getElementById('ket-qua-bai-5').innerHTML = 'Số chẵn cuối cùng của mảng là: ' + ketQua;
@@ -73,20 +73,100 @@ document.getElementById('btn-bai-5').onclick = function(){
 }
 
 //Bài tập 6: Đổi chỗ 2 vị trí trong mảng
-document.getElementById('btn-bai-6').onclick = function(){
+document.getElementById('btn-bai-6').onclick = function () {
     //input: arrNum, viTri1(number), viTri2(number)
     var viTri1 = document.getElementById('vi-tri-1').value;
     var viTri2 = document.getElementById('vi-tri-2').value;
-    //output: ketQua
-    
+    //output: arrNum
+
     //process:
-    if (viTri1===''||viTri2===''){
-        document.getElementById('ket-qua-bai-6').innerHTML ='Vui lòng nhập vị trí cần đổi!'
-    } else if (viTri1>=arrNum.length||viTri2>=arrNum.length){
+    if (viTri1 === '' || viTri2 === '') {
+        document.getElementById('ket-qua-bai-6').innerHTML = 'Vui lòng nhập vị trí cần đổi!'
+    } else if (viTri1 >= arrNum.length || viTri2 >= arrNum.length) {
         document.getElementById('ket-qua-bai-6').innerHTML = 'Vị trí không khả dụng! Mảng hiện tại là: ' + '[ ' + arrNum + ' ]';
     } else {
-        doiViTri(arrNum,viTri1,viTri2);
+        doiViTri(arrNum, viTri1, viTri2);
         document.getElementById('ket-qua-bai-6').innerHTML = '[ ' + arrNum + ' ]';
     }
-    
+
 }
+
+//Bài tập 7: sắp xếp mảng theo thứ tự tăng dần
+document.getElementById('btn-bai-7').onclick = function () {
+    //input: arrNum
+    //output: ketQua
+    var ketQua = '';
+    //process:
+    ketQua = sapXep(arrNum);
+    document.getElementById('ket-qua-bai-7').innerHTML = '[ ' + ketQua + ' ]';
+}
+
+//Bài tập 8: tìm số nguyên tố đầu tiên:
+document.getElementById('btn-bai-8').onclick = function () {
+    //input: arrNum
+    //output: ketQua
+    var ketQua = '';
+    //process:
+    for (var index = 0; index < arrNum.length; index++) {
+        var output = kiemTraSoNguyenTo(arrNum[index]);
+        if (output === true) {
+            ketQua = arrNum[index];
+            break;
+        }
+    }
+    if (ketQua === '') {
+        document.getElementById('ket-qua-bai-8').innerHTML = 'Mảng không có số nguyên tố!'
+    } else {
+        document.getElementById('ket-qua-bai-8').innerHTML = 'Số nguyên tố đầu tiên của mảng là: ' + ketQua;
+    }
+
+}
+
+//Bài tập 9: Đếm số nguyên
+// Nhập số tạo mảng:
+var arrNumBai9 = [];
+document.getElementById('btn-tao-mang-bai-9').onclick = function () {
+    var num = document.getElementById('num-bai-9').value;
+    if (num === '') {
+        num = 0;
+    }
+    arrNumBai9.push(Number(num));
+    document.getElementById('hien-thi-mang-bai-9').innerHTML = '[ ' + arrNumBai9 + ' ]';
+
+}
+document.getElementById('btn-bai-9').onclick = function () {
+    //input: arrNumBai9
+
+    //output: ketQua
+    var ketQua = 0;
+    //process:
+    //Đếm số lượng
+    for (var index = 0; index < arrNumBai9.length; index++) {
+        var output =Number.isInteger(arrNumBai9[index]);
+        if (output === true) {
+            ketQua += 1;
+        }
+    }
+    //In kết quả:
+    
+    document.getElementById('ket-qua-bai-9').innerHTML = 'Số lượng số nguyên của mảng là: ' + ketQua;
+
+}
+
+//Bài 10: So sánh số lượng số âm và số dương
+document.getElementById('btn-bai-10').onclick = function(){
+    //input: arrNum
+    //Output: ketQua
+    var ketQua = '';
+    //process:
+    var soSoDuong = demSoDuong(arrNum);
+    var soSoAm = demSoAm(arrNum);
+    if (soSoDuong>soSoAm){
+        document.getElementById('ket-qua-bai-10').innerHTML = 'Số dương > số âm';
+    } else if (soSoDuong===soSoAm){
+        document.getElementById('ket-qua-bai-10').innerHTML = 'Số dương = số âm';
+    } else {
+        document.getElementById('ket-qua-bai-10').innerHTML = 'Số dương < số âm';
+    }
+}
+    
--- js/methods.js
+++ js/methods.js
@@ -27,6 +27,16 @@ function demSoDuong(arr) {
     return output;
 }
 
+function demSoAm(arr) {
+    var output = 0;
+    for (var index = 0; index < arr.length; index++) {
+        if (arr[index] < 0) {
+            output++;
+        }
+    }
+    return output;
+}
+
 function timSoNhoNhat(arr) {
     var output = arr[0];
     for (var index = 1; index < arr.length; index++) {
@@ -57,19 +67,68 @@ function soDuongNhoNhat(arr) {
  */
 function soChanCuoiCung(arr) {
     var output = -1;
-    for (var index = 0; index < arr.length; index++){
-        if (arr[index]%2===0){
+    for (var index = 0; index < arr.length; index++) {
+        if (arr[index] % 2 === 0) {
             output = arr[index];
         }
     }
     return output;
 }
-
-function doiViTri(arr,num1,num2){
-    var output='';
+/**
+ * Hàm thay đổi vị trí của 2 phần tử trong mảng
+ * @param {*} arr mảng người dùng nhập vào
+ * @param {*} num1 vị trí thứ 1 cần thay đổi sang vị trí thứ 2
+ * @param {*} num2 vị trí thứ 2 cần thay đổi sang vị trí thứ 1
+ */
+function doiViTri(arr, num1, num2) {
+    var output = '';
     var giaTri1 = arr[num1];
     var giaTri2 = arr[num2]
-    arr[num1]=giaTri2;
-    arr[num2]=giaTri1;
+    arr[num1] = giaTri2;
+    arr[num2] = giaTri1;
     output = arr;
+}
+/**
+ * Hàm sắp xếp các phần tử của mảng theo thứ tự tăng dần giá trị
+ * @param {*} arr mảng người dùng nhập vào
+ * @returns trả về mảng đã sắp xếp
+ */
+function sapXep(arr) {
+
+    for (var index = 0; index < arr.length; index++) {
+        for (var index2 = index + 1; index2 < arr.length; index2++) {
+            if (arr[index] > arr[index2]) {
+                //Hoan doi 2 vi tri arr[index] và arr[index2]
+                var trungGian = arr[index];
+                arr[index] = arr[index2];
+                arr[index2] = trungGian;
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+/**
+ * Hàm kiểm tra 1 số có phải là số nguyên tố hay không
+ * @param {*} num số cần kiểm tra
+ * @returns boolean: true = số nguyên tố, false = không phải số nguyên tố.
+ */
+function kiemTraSoNguyenTo(num) {
+
+    var output = true;
+    var uoc = 2;
+    if (num <= 1) {
+        output = false;
+    } else {
+        while (uoc <= Math.sqrt(num)) {
+            if (num % uoc === 0) {
+                output = false;
+                break;
+            }
+            uoc++;
+        }
+    }
+    return output;
 }
